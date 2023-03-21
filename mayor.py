2cc0f0eb4244917ce0e84c56ad3e5eb3da7613ff
Rafa3ldom1nguez <125380885+Rafa3ldom1nguez@users.noreply.github.com>
2023-03-21 10:24:36 -0600

    agregamos un nuevo programa
    el programa verifica e numero mayor

---
+++ mayor.py
@@ -0,0 +1,8 @@
+num = int(input("Ingrese un numero "))
+
+numDos = int(input("Ingrese un numero "))
+
+if num > numDos:
+    print("El primer numero es el mayor")
+else:
+    print("El segundo numero es el mayor")
